3491c01a44e99b5c6b94471070e59c39c4f12c62
Roland Arsenault <roland@ccom.unh.edu>
2018-12-20 17:11:24 -0500

    Added ability to split multiple lines that are group in single packet.

--- nodes/posmv_nmea_node.py
+++ nodes/posmv_nmea_node.py
@@ -39,59 +39,61 @@ def posmv_nmea_listener():
 
     while not rospy.is_shutdown():
         if input_type == 'serial':
-            nmea_in = serial_in.readline()
+            nmea_ins = (serial_in.readline(),)
             #print nmea_in
             if udp_out is not None:
                 udp_out.sendto(nmea_in, (output_address,output_port))
         else:
-            nmea_in,addr = udp_in.recvfrom(1024)
-            #print addr, nmea_in
-        now = rospy.get_rostime()    
-        nmea_parts = nmea_in.strip().split(',')
-        if len(nmea_parts):
-            #print nmea_parts
-            try:
-                if nmea_parts[0] == '$GPZDA' and len(nmea_parts) >= 5:
-                    tref = TimeReference()
-                    tref.header.stamp = now
-                    hour = int(nmea_parts[1][0:2])
-                    minute = int(nmea_parts[1][2:4])
-                    second = int(nmea_parts[1][4:6])
-                    ms = int(float(nmea_parts[1][6:])*1000000)
-                    day = int(nmea_parts[2])
-                    month = int(nmea_parts[3])
-                    year = int(nmea_parts[4])
-                    zda = datetime.datetime(year,month,day,hour,minute,second,ms)
-                    tref.time_ref = rospy.Time(calendar.timegm(zda.timetuple()),zda.microsecond*1000)
-                    tref.source = 'posmv'
-                    timeref_pub.publish(tref)
-                    bag.write('/posmv_nmea/time_reference', tref)
-                if nmea_parts[0] == '$GPGGA' and len(nmea_parts) >= 10:
-                    latitude = int(nmea_parts[2][0:2])+float(nmea_parts[2][2:])/60.0
-                    if nmea_parts[3] == 'S':
-                        latitude = -latitude
-                    longitude = int(nmea_parts[4][0:3])+float(nmea_parts[4][3:])/60.0
-                    if nmea_parts[5] == 'W':
-                        longitude = -longitude
-                    altitude = float(nmea_parts[9])
-                    nsf = NavSatFix()
-                    nsf.header.stamp = now
-                    nsf.header.frame_id = 'posmv_operator'
-                    nsf.latitude = latitude
-                    nsf.longitude = longitude
-                    nsf.altitude = altitude
-                    position_pub.publish(nsf)
-                    bag.write('/posmv_nmea/position', nsf)
-                if nmea_parts[0] == '$GPHDT' and len(nmea_parts) >= 2:
-                    heading = float(nmea_parts[1])
-                    nes = NavEulerStamped()
-                    nes.header.stamp = now
-                    nes.header.frame_id = 'posmv_operator'
-                    nes.orientation.heading = heading
-                    orientation_pub.publish(nes)
-                    bag.write('/posmv_nmea/orientation', nes)
-            except ValueError:
-                pass
+            nmea_in,addr = udp_in.recvfrom(2048)
+            print addr, nmea_in
+            nmea_ins = nmea_in.split('\n')
+        now = rospy.get_rostime()
+        for nmea_in in nmea_ins:
+            nmea_parts = nmea_in.strip().split(',')
+            if len(nmea_parts):
+                #print nmea_parts
+                try:
+                    if nmea_parts[0] == '$GPZDA' and len(nmea_parts) >= 5:
+                        tref = TimeReference()
+                        tref.header.stamp = now
+                        hour = int(nmea_parts[1][0:2])
+                        minute = int(nmea_parts[1][2:4])
+                        second = int(nmea_parts[1][4:6])
+                        ms = int(float(nmea_parts[1][6:])*1000000)
+                        day = int(nmea_parts[2])
+                        month = int(nmea_parts[3])
+                        year = int(nmea_parts[4])
+                        zda = datetime.datetime(year,month,day,hour,minute,second,ms)
+                        tref.time_ref = rospy.Time(calendar.timegm(zda.timetuple()),zda.microsecond*1000)
+                        tref.source = 'posmv'
+                        timeref_pub.publish(tref)
+                        bag.write('/posmv_nmea/time_reference', tref)
+                    if nmea_parts[0] == '$GPGGA' and len(nmea_parts) >= 10:
+                        latitude = int(nmea_parts[2][0:2])+float(nmea_parts[2][2:])/60.0
+                        if nmea_parts[3] == 'S':
+                            latitude = -latitude
+                        longitude = int(nmea_parts[4][0:3])+float(nmea_parts[4][3:])/60.0
+                        if nmea_parts[5] == 'W':
+                            longitude = -longitude
+                        altitude = float(nmea_parts[9])
+                        nsf = NavSatFix()
+                        nsf.header.stamp = now
+                        nsf.header.frame_id = 'posmv_operator'
+                        nsf.latitude = latitude
+                        nsf.longitude = longitude
+                        nsf.altitude = altitude
+                        position_pub.publish(nsf)
+                        bag.write('/posmv_nmea/position', nsf)
+                    if nmea_parts[0] == '$GPHDT' and len(nmea_parts) >= 2:
+                        heading = float(nmea_parts[1])
+                        nes = NavEulerStamped()
+                        nes.header.stamp = now
+                        nes.header.frame_id = 'posmv_operator'
+                        nes.orientation.heading = heading
+                        orientation_pub.publish(nes)
+                        bag.write('/posmv_nmea/orientation', nes)
+                except ValueError:
+                    pass
     bag.close()
         
             
