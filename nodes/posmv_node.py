04d2b472579b62a26afc05bff3115261bcbcbd5a
Roland Arsenault <roland@ccom.unh.edu>
2021-05-13 08:50:57 -0400

    Check for nav status

--- nodes/posmv_node.py
+++ nodes/posmv_node.py
@@ -5,6 +5,7 @@ from past.utils import old_div
 from posmv import posmv
 import rospy
 from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import NavSatStatus
 from sensor_msgs.msg import Imu
 from sensor_msgs.msg import TimeReference
 from geometry_msgs.msg import TwistWithCovarianceStamped
@@ -69,6 +70,8 @@ def posmv_listener():
   last_group_2_diag_time = None
   last_diag_time = None
 
+  navsat_status = None
+  
 
 
   while not rospy.is_shutdown():
@@ -87,59 +90,62 @@ def posmv_listener():
                 tref.source = posmv_frame
                 timeref_pub.publish(tref)
                 
-            nsf = NavSatFix()
-            nsf.header.stamp = now
-            nsf.header.frame_id = posmv_frame
-            nsf.latitude = d['latitude']
-            nsf.longitude = d['longitude']
-            nsf.altitude = d['altitude']
-            if group_2 is not None:
-              nsf.position_covariance[0] = group_2['east_position_rms_error']**2
-              nsf.position_covariance[4] = group_2['north_position_rms_error']**2
-              nsf.position_covariance[8] = group_2['down_position_rms_error']**2
-              nsf.position_covariance_type = NavSatFix.COVARIANCE_TYPE_DIAGONAL_KNOWN
-            position_pub.publish(nsf)
+            if navsat_status is not None:
+              
+              nsf = NavSatFix()
+              nsf.status = navsat_status
+              nsf.header.stamp = now
+              nsf.header.frame_id = posmv_frame
+              nsf.latitude = d['latitude']
+              nsf.longitude = d['longitude']
+              nsf.altitude = d['altitude']
+              if group_2 is not None:
+                nsf.position_covariance[0] = group_2['east_position_rms_error']**2
+                nsf.position_covariance[4] = group_2['north_position_rms_error']**2
+                nsf.position_covariance[8] = group_2['down_position_rms_error']**2
+                nsf.position_covariance_type = NavSatFix.COVARIANCE_TYPE_DIAGONAL_KNOWN
+              position_pub.publish(nsf)
 
-            imu = Imu()                  
-            imu.header.stamp = now
-            imu.header.frame_id = posmv_frame
-            q = tf.transformations.quaternion_from_euler(math.radians(90.0-d['vessel_heading']), math.radians(-d['vessel_pitch']), math.radians(d['vessel_roll']), 'rzyx')
-            
-            imu.orientation.x = q[0]
-            imu.orientation.y = q[1]
-            imu.orientation.z = q[2]
-            imu.orientation.w = q[3]
-            if group_2 is not None:
-              imu.orientation_covariance[0] = math.radians(group_2['roll_rms_error']**2)
-              imu.orientation_covariance[4] = math.radians(group_2['pitch_rms_error']**2)
-              imu.orientation_covariance[8] = math.radians(group_2['heading_rms_error']**2)
-            imu.angular_velocity.x = math.radians(d['vessel_angular_rate_about_longitudinal_axis'])
-            imu.angular_velocity.y = math.radians(-d['vessel_angular_rate_about_transverse_axis'])
-            imu.angular_velocity.z = math.radians(-d['vessel_angular_rate_about_down_axis'])
-            imu.linear_acceleration.x = d['vessel_longitudinal_acceleration']
-            imu.linear_acceleration.y = d['vessel_transverse_acceleration']
-            imu.linear_acceleration.z = -d['vessel_down_acceleration']
-            
-            #todo: measure and calculate covariance
-            # From Val on teams:
-            # This is a bit of a guess, but try (4 \sigma^2) / dt^2, where sigma^2 is the variance of the angle (rms^2) and dt is the time interval.
-            # This is based on a commonly used "discrete noise process model" used to model the uncertainty for the model part of a Kalman Filter. I think it should be a reasonable estimate. (ref: Estimation with Applications to Tracking and Navigation")￼
-            # It might be good to annotate that in a comment. Section 3.3.3, page 274. Authors are Bar-Shalom, Li and Kirubarajan
+              imu = Imu()                  
+              imu.header.stamp = now
+              imu.header.frame_id = posmv_frame
+              q = tf.transformations.quaternion_from_euler(math.radians(90.0-d['vessel_heading']), math.radians(-d['vessel_pitch']), math.radians(d['vessel_roll']), 'rzyx')
+              
+              imu.orientation.x = q[0]
+              imu.orientation.y = q[1]
+              imu.orientation.z = q[2]
+              imu.orientation.w = q[3]
+              if group_2 is not None:
+                imu.orientation_covariance[0] = math.radians(group_2['roll_rms_error']**2)
+                imu.orientation_covariance[4] = math.radians(group_2['pitch_rms_error']**2)
+                imu.orientation_covariance[8] = math.radians(group_2['heading_rms_error']**2)
+              imu.angular_velocity.x = math.radians(d['vessel_angular_rate_about_longitudinal_axis'])
+              imu.angular_velocity.y = math.radians(-d['vessel_angular_rate_about_transverse_axis'])
+              imu.angular_velocity.z = math.radians(-d['vessel_angular_rate_about_down_axis'])
+              imu.linear_acceleration.x = d['vessel_longitudinal_acceleration']
+              imu.linear_acceleration.y = d['vessel_transverse_acceleration']
+              imu.linear_acceleration.z = -d['vessel_down_acceleration']
+              
+              #todo: measure and calculate covariance
+              # From Val on teams:
+              # This is a bit of a guess, but try (4 \sigma^2) / dt^2, where sigma^2 is the variance of the angle (rms^2) and dt is the time interval.
+              # This is based on a commonly used "discrete noise process model" used to model the uncertainty for the model part of a Kalman Filter. I think it should be a reasonable estimate. (ref: Estimation with Applications to Tracking and Navigation")￼
+              # It might be good to annotate that in a comment. Section 3.3.3, page 274. Authors are Bar-Shalom, Li and Kirubarajan
 
-            orientation_pub.publish(imu)
+              orientation_pub.publish(imu)
 
-            twcs = TwistWithCovarianceStamped()
-            twcs.header.stamp = now
-            twcs.header.frame_id = posmv_frame
-            twcs.twist.twist.linear.x = d['east_velocity']
-            twcs.twist.twist.linear.y = d['north_velocity']
-            twcs.twist.twist.linear.z = -d['down_velocity']
-            if group_2 is not None:
-              twcs.twist.covariance[0] = group_2['east_velocity_rms_error']**2
-              twcs.twist.covariance[7] = group_2['north_velocity_rms_error']**2
-              twcs.twist.covariance[14] = group_2['down_velocity_rms_error']**2
-            twcs.twist.twist.angular = imu.angular_velocity
-            velocity_pub.publish(twcs)
+              twcs = TwistWithCovarianceStamped()
+              twcs.header.stamp = now
+              twcs.header.frame_id = posmv_frame
+              twcs.twist.twist.linear.x = d['east_velocity']
+              twcs.twist.twist.linear.y = d['north_velocity']
+              twcs.twist.twist.linear.z = -d['down_velocity']
+              if group_2 is not None:
+                twcs.twist.covariance[0] = group_2['east_velocity_rms_error']**2
+                twcs.twist.covariance[7] = group_2['north_velocity_rms_error']**2
+                twcs.twist.covariance[14] = group_2['down_velocity_rms_error']**2
+              twcs.twist.twist.angular = imu.angular_velocity
+              velocity_pub.publish(twcs)
 
             ds = DiagnosticStatus()
             ds.name = 'posmv/alignment_status'
@@ -190,6 +196,18 @@ def posmv_listener():
             gps_utc_offset = d['gps_utc_time_offset']
             #print 'gps week:', gps_week, 'offset:', gps_utc_offset, 'time types:', d['time_types']
             #print decode_time(d,gps_week,gps_utc_offset)
+
+            if navsat_status is None:
+              navsat_status = NavSatStatus()
+              navsat_status.service = NavSatStatus.SERVICE_GPS
+            navsat_status.status = NavSatStatus.STATUS_NO_FIX
+            if d['navigation_solution_status'] in (1, 2, 8):
+              navsat_status.status = NavSatStatus.STATUS_FIX
+            if d['navigation_solution_status'] in (3, 4, 5, 6, 7):
+              navsat_status.status = NavSatStatus.STATUS_GBAS_FIX
+            if d['navigation_solution_status'] in (9, 10, 11, 12, 13, 15):
+              navsat_status.status = NavSatStatus.STATUS_FIX
+
 
     if last_diag_time is None or now - last_diag_time > rospy.Duration(1.0):
       last_diag_time = now
